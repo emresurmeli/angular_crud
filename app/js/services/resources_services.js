19b4d70fb4593a9f5c727e0a8bc2fae7dd738539
Emre Surmeli <s.emre.s.8@gmail.com>
2015-06-25 16:59:50 -0700

    fix white space

--- app/js/services/resources_services.js
+++ app/js/services/resources_services.js
@@ -2,43 +2,43 @@
 
 module.exports = function(app) {
 
-	var handleError = function(data) {
-		console.log(data);
-	};
+  var handleError = function(data) {
+    console.log(data);
+  };
 
-	app.factory('resource', ['$http', function($http) {
-		return function(resourceName) {
-			return {
-				getAll: function(callback) {
-					$http({
-						method: 'GET',
-						url: '/api/' + resourceName
-					}).success(callback).error(handleError);
-				},
+  app.factory('resource', ['$http', function($http) {
+    return function(resourceName) {
+      return {
+        getAll: function(callback) {
+          $http({
+            method: 'GET',
+            url: '/api/' + resourceName
+          }).success(callback).error(handleError);
+        },
 
-				create: function(resource, callback) {
-					$http({
-						method: 'POST',
-						url: '/api/' + resourceName,
-						data: resource
-					}).success(callback).error(handleError);
-				},
+        create: function(resource, callback) {
+          $http({
+            method: 'POST',
+            url: '/api/' + resourceName,
+            data: resource
+          }).success(callback).error(handleError);
+        },
 
-				save: function(resource, callback) {
-					$http({
-						method: 'PUT',
-						url: '/api/' + resourceName + '/' + resource._id,
-						data: resource
-					}).success(callback).error(handleError);
-				},
+        save: function(resource, callback) {
+          $http({
+            method: 'PUT',
+            url: '/api/' + resourceName + '/' + resource._id,
+            data: resource
+          }).success(callback).error(handleError);
+        },
 
-				remove: function(resource, callback) {
-					$http({
-						method: 'DELETE',
-						url: '/api/' + resourceName + '/' + resource._id
-					}).success(callback).error(handleError);
-				}
-			};
-		};
-	}]);
+        remove: function(resource, callback) {
+          $http({
+            method: 'DELETE',
+            url: '/api/' + resourceName + '/' + resource._id
+          }).success(callback).error(handleError);
+        }
+      };
+    };
+  }]);
 };
